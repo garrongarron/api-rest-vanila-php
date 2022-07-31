9e668689c53c7f25651af984bb0314dfb7ede949
Federico Zacayan <federico.zacayan@gmail.com>
2022-07-31 17:17:10 -0300

    endpoint matching

--- api/core/Request.php
+++ api/core/Request.php
@@ -1,8 +1,10 @@
 <?php
+
 function getId()
 {
     return explode("/", $_SERVER['REQUEST_URI'])[2] || null;
 }
+
 function getBody(){
     return json_decode(file_get_contents('php://input'));
 }
--- api/core/Router.php
+++ api/core/Router.php
@@ -3,6 +3,9 @@
 function router($endpoint, $callback)
 {
     return function($method) use( &$endpoint, &$callback){
+        if (substr($_SERVER['REQUEST_URI'], 0, strlen($endpoint)) != $endpoint) {
+            return
+        }
         if($_SERVER['REQUEST_METHOD'] == $method){
             echo $callback();
         }
